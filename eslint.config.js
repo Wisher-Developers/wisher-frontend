625efd420e9bbeb0574996bb513f1fee7142b9ba
Michael Gurevich <michael.gurevich.02@mail.ru>
2024-11-02 02:18:56 +0300

    lint: add import order

--- eslint.config.js
+++ eslint.config.js
@@ -1,44 +1,82 @@
 import js from "@eslint/js"
+import tseslint from "@typescript-eslint/eslint-plugin"
+import tsParser from "@typescript-eslint/parser"
+import importPlugin from "eslint-plugin-import"
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended"
 import react from "eslint-plugin-react"
 import reactHooks from "eslint-plugin-react-hooks"
 import reactRefresh from "eslint-plugin-react-refresh"
 import globals from "globals"
-import tseslint from "typescript-eslint"
 
-export default tseslint.config(
-  { ignores: ["dist"] },
+export default [
   eslintPluginPrettierRecommended,
   {
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.recommendedTypeChecked,
-      ...tseslint.configs.stylisticTypeChecked,
-    ],
-    files: ["**/*.{ts,tsx}"],
+    files: ["**/*.ts", "**/*.tsx"],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+      },
+      parser: tsParser,
       parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: "module",
         project: ["./tsconfig.node.json", "./tsconfig.app.json"],
-        tsconfigRootDir: import.meta.dirname,
       },
     },
-    settings: { react: { version: "18.3" } },
     plugins: {
-      react,
+      "@typescript-eslint": tseslint,
+      "import": importPlugin,
+      "react": react,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
     },
     rules: {
+      ...js.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+      ...importPlugin.flatConfigs.recommended.rules,
       ...react.configs.recommended.rules,
-      ...react.configs["jsx-runtime"].rules,
       ...reactHooks.configs.recommended.rules,
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
       ],
+      "react/react-in-jsx-scope": "off",
       "sort-imports": ["error", { ignoreDeclarationSort: true }],
+      "import/order": [
+        "error",
+        {
+          "groups": ["builtin", "external", "internal", "sibling"],
+          "pathGroups": [
+            {
+              pattern: "react",
+              group: "builtin",
+              position: "before",
+            },
+            {
+              pattern: "src/**",
+              group: "internal",
+              position: "before",
+            },
+          ],
+          "pathGroupsExcludedImportTypes": ["react"],
+          "newlines-between": "always",
+          "alphabetize": {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
     },
-  }
-)
+    settings: {
+      "react": {
+        version: "detect",
+      },
+      "import/resolver": {
+        typescript: {
+          alwaysTryTypes: true,
+          project: ["./tsconfig.node.json", "./tsconfig.app.json"],
+        },
+      },
+    },
+  },
+]
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,9 @@
 import { useState } from "react"
-import reactLogo from "~/assets/react.svg"
-import viteLogo from "/vite.svg"
-import "./App.css"
+
 import Button from "@/components/Button"
+import reactLogo from "~/assets/react.svg"
+
+import "./App.css"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,7 +12,7 @@ function App() {
     <>
       <div>
         <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
