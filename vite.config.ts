6266dd2f3e48671dc5e469959e9ae25e92b2e16d
Michael Gurevich <michael.gurevich.02@mail.ru>
2024-11-02 01:34:10 +0300

    chore: setup paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
+import path from "path"
+
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   base: "/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src/app"),
+      "~": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     port: 3000,
   },
