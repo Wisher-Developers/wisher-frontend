8ece39a91dc828946102a20ecd04bc4b86dea135
Michael Gurevich <michael.gurevich.02@mail.ru>
2024-11-03 12:51:29 +0300

    fix: allow http

--- src/shared/api/query.ts
+++ src/shared/api/query.ts
@@ -9,14 +9,7 @@ export const BASE_URL = "http://193.178.170.139:8080/api"
 
 const baseQuery = fetchBaseQuery({
   baseUrl: BASE_URL,
-  mode: "cors",
-  prepareHeaders: headers => {
-    const next = new Headers(headers)
-
-    next.set("Content-Security-Policy", "upgrade-insecure-requests")
-
-    return next
-  },
+  referrerPolicy: "unsafe-url",
 })
 
 export const apiBaseQuery: BaseQueryFn<
