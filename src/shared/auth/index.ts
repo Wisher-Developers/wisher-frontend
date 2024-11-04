4520abb541c8abaa4123489a990b16f91537f8d7
Michael Gurevich <michael.gurevich.02@mail.ru>
2024-11-04 16:40:23 +0300

    feat: add token utils

--- src/shared/auth/index.ts
+++ src/shared/auth/index.ts
@@ -1,16 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit"
 import Cookies from "js-cookie"
 
+import { TOKEN_COOKIE, loadToken, removeToken } from "./token"
 import { AuthSlice } from "./types"
 
 const initialState: AuthSlice = {
-  token: Cookies.get("token"),
+  token: Cookies.get(TOKEN_COOKIE),
 }
 
 const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {},
+  extraReducers: builder => {
+    builder.addCase(loadToken.fulfilled, (state, { payload }) => {
+      state.token = payload
+    })
+
+    builder.addCase(removeToken.fulfilled, state => {
+      state.token = undefined
+    })
+  },
 })
 
 export default authSlice.reducer
