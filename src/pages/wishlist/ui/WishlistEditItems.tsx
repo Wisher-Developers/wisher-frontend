b353054154948f80c930d82fe37317c3db37cd15
Michael Gurevich <michael.gurevich.02@mail.ru>
2024-11-09 23:18:31 +0300

    feat: add hover effect

--- src/pages/wishlist/ui/WishlistEditItems.tsx
+++ src/pages/wishlist/ui/WishlistEditItems.tsx
@@ -4,7 +4,9 @@ import styled from "styled-components"
 
 import { useGetWishitemsQuery } from "@entities/wishitem/api"
 import WishitemPreview from "@entities/wishitem/ui/WishitemPreview"
+import DeleteWishitemButton from "@features/delete-wishitem/ui/DeleteWishitemButton"
 import CreateWishitemButton from "@features/upsert-wishitem/ui/CreateWishitemButton"
+import EditWishitemButton from "@features/upsert-wishitem/ui/EditWishitemButton"
 import Container from "@shared/ui/Container"
 
 export default function WishlistEditItems() {
@@ -19,7 +21,14 @@ export default function WishlistEditItems() {
       <CreateWishitemButton />
 
       {wishitems.map(wishitem => (
-        <StyledWishitemPreview key={wishitem.id} wishitem={wishitem} />
+        <WishitemWrapper key={wishitem.id}>
+          <WishitemPreview wishitem={wishitem} />
+
+          <Hidden>
+            <EditWishitemButton wishitem={wishitem} />
+            <DeleteWishitemButton wishitem={wishitem} />
+          </Hidden>
+        </WishitemWrapper>
       ))}
     </Wrapper>
   )
@@ -34,7 +43,35 @@ const Wrapper = styled(Container)`
   gap: 16px;
 `
 
-const StyledWishitemPreview = styled(WishitemPreview)`
-  &:hover {
+const Hidden = styled.div`
+  border-radius: 32px;
+  background: rgba(255, 255, 255, 0.6);
+  backdrop-filter: blur(32px);
+
+  width: 100%;
+  height: 100%;
+  padding: 16px;
+  box-sizing: border-box;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: stretch;
+  gap: 8px;
+
+  position: absolute;
+  top: 0;
+
+  opacity: 0;
+  transition: opacity var(--transition-duration) var(--transition-function);
+`
+
+const WishitemWrapper = styled.div`
+  position: relative;
+  height: fit-content;
+  width: fit-content;
+
+  &:hover ${Hidden} {
+    opacity: 1;
   }
 `
