f252f7f5b0e34ebae1d14b107f533cb35395bc59
Michael Gurevich <michael.gurevich.02@mail.ru>
2024-11-09 18:51:43 +0300

    feat: add auth headers to requests

--- src/shared/api/query.ts
+++ src/shared/api/query.ts
@@ -5,14 +5,26 @@ import {
   fetchBaseQuery,
 } from "@reduxjs/toolkit/query/react"
 
+import { RootState } from "@app/model/store"
 import { withQueryParams } from "@shared/lib/withQueryParams"
 
-export const BASE_URL = "http://localhost:8080/api"
+export const BASE_URL = "http://193.178.170.139/api"
 
 const baseQuery = fetchBaseQuery({
   baseUrl: BASE_URL,
   mode: "cors",
   paramsSerializer: withQueryParams,
+  prepareHeaders: (headers, api) => {
+    const {
+      auth: { token },
+    } = api.getState() as RootState
+
+    const next = new Headers(headers)
+
+    if (token) next.set("Authorization", `Bearer ${token}`)
+
+    return next
+  },
 })
 
 export const apiBaseQuery: BaseQueryFn<
